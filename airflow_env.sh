ea6897f2f5f5672399305fc3ff640551a500edd0
Sai Thiha Zaw <sai.t.zaw@frontiir.net>
2022-02-05 08:32:58 +0630

    remove the file permission

--- airflow_env.sh
+++ airflow_env.sh
@@ -1,12 +1,1 @@
 echo -e "AIRFlOW_UID=$(id -u)\nAIRFLOW_GID=0">.env
-
-DIR1="/logs"
-DIR2="/data"
-
-if [ -d $DIR1 ]; then 
-	sudo chmod 764 $DIR1
-fi
-
-if [ -d $DIR2 ]; then
-	sudo chmod 764 $DIR2
-fi
